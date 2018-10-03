e11537e5994a30d7f6ff5bcc2b18cb6a873c8c43
Botifier <mouath.sai.ensi@gmail.com>
2018-10-03 07:25:58 +0100

    Add benchmarking for cache

--- webapp/fibonacci.py
+++ webapp/fibonacci.py
@@ -17,5 +17,33 @@ class Fibonacci(object):
         self.__compute(end_idx)
         return self._results[start_idx:end_idx+1]
 
-if __name__ == '__main__': # TODO: add benchmarking for cache
-  pass
+if __name__ == '__main__': 
+  from time import time
+
+  def timeit(foo):
+    def func():
+      start = time()
+      foo()
+      end = time()
+      t = (end - start) * 1000
+      print ("execution in ms: {}".format(t))
+    return func
+  
+  @timeit
+  def cached():
+    fib = Fibonacci()
+    fib.sequence(1, 10000)
+    fib.sequence(1, 10001)
+
+  @timeit
+  def not_cached():
+    fib = Fibonacci()
+    fib.sequence(1, 10000)
+    # we remove the cached values
+    fib._results = [0, 1]
+    fib.sequence(1, 10001)
+
+  print("with cache")
+  cached()
+  print("without cache")
+  not_cached()
