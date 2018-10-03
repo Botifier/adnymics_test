f46ffd5bfd09bf19f36cb81cae3d8236c00b08b0
Botifier <mouath.sai.ensi@gmail.com>
2018-10-03 07:43:10 +0100

    fix identation

--- webapp/fibonacci.py
+++ webapp/fibonacci.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 try:
     xrange
@@ -17,33 +19,34 @@ class Fibonacci(object):
         self.__compute(end_idx)
         return self._results[start_idx:end_idx+1]
 
+
 if __name__ == '__main__': 
-  from time import time
+    from time import time
 
-  def timeit(foo):
-    def func():
-      start = time()
-      foo()
-      end = time()
-      t = (end - start) * 1000
-      print ("execution in ms: {}".format(t))
-    return func
-  
-  @timeit
-  def cached():
-    fib = Fibonacci()
-    fib.sequence(1, 10000)
-    fib.sequence(1, 10001)
+    def timeit(foo):
+        def func():
+            start = time()
+            foo()
+            end = time()
+            t = (end - start) * 1000
+            print ("execution in ms: {}".format(t))
+        return func
 
-  @timeit
-  def not_cached():
-    fib = Fibonacci()
-    fib.sequence(1, 10000)
-    # we remove the cached values
-    fib._results = [0, 1]
-    fib.sequence(1, 10001)
+    @timeit
+    def cached():
+        fib = Fibonacci()
+        fib.sequence(1, 10000)
+        fib.sequence(1, 10001)
 
-  print("with cache")
-  cached()
-  print("without cache")
-  not_cached()
+    @timeit
+    def not_cached():
+        fib = Fibonacci()
+        fib.sequence(1, 10000)
+        # we remove the cached values
+        fib._results = [0, 1]
+        fib.sequence(1, 10001)
+
+    print("with cache")
+    cached()
+    print("without cache")
+    not_cached()
